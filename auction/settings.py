8f67874fe70e9a171f4b1e12495f3732f85c4499
Kuldeep Singh <2019083@iiitdmj.ac.in>
2021-09-22 16:22:08 +0530

    improved db

--- auction/settings.py
+++ auction/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'auction.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'auction',
         'USER': 'root',
         'PASSWORD': '',
